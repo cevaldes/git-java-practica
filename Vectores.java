849d6dc83ec85b14b3cd5214c4096ca1580ada8e
cevaldes <cevaldes.arias@gmail.com>
2019-10-13 10:26:12 -0500

    v1 Vectores.java

---
+++ Vectores.java
@@ -0,0 +1,19 @@
+public class Vectores{
+  public static void main(String args[]){
+ 
+   int numeros[] = new int[5];
+
+   numeros[0] = 5;
+   numeros[1] = 220;
+   numeros[2] = 8;
+   numeros[3] = 458;
+   numeros[4] = 22;
+
+   for (int i=0; i<=4; i++){
+  
+      System.out.print("[" + numeros[i] + "]");
+
+   }
+
+ }
+}
